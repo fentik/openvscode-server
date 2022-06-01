8c1e38812262d9f41f96cc6f429e92ae8bbd0d91
Sergei Tsarev <sergei@fentik.com>
2022-06-01 19:35:12 +0000

    add docker build scaffolding

---
+++ docker/build_ide_server_docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Builds dataflo-ide-server docker.
+
+set -e
+
+ECR_HOST="460511261886.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REPOSITORY="dev-dataflo-ide-server"
+GIT_BRANCH='fentik'
+ECR_REGION='us-east-1'
+DEPLOY_ENV='dev'
+
+while :; do
+    case $1 in
+        --prod)
+            DEPLOY_ENV="prod"
+            ;;
+        --dev)
+            DEPLOY_ENV="dev"
+            ;;
+        --no-push)
+            NO_PUSH='TRUE'
+            ;;
+        --local)
+            LOCAL='TRUE'
+            ;;
+        *)
+            break
+    esac
+    shift
+done
+
+if [ $DEPLOY_ENV == "prod" ]; then
+    ECR_REPOSITORY="prod-dataflo-ide-server"
+    GIT_BRANCH="main"
+fi
+
+
+ECR_REPO_FQN="$ECR_HOST/$ECR_REPOSITORY"
+DOCKER_FILE_PATH="docker/Dockerfile"
+
+PLATFORM=`uname -m`
+
+if [ ! $LOCAL ]; then
+    TMP_DIR=$(mktemp -d)
+    echo "Using $TMP_DIR to build docker image..."
+    cd $TMP_DIR
+    git clone -b $GIT_BRANCH git@github.com:fentik/dataflo.git
+    cd dataflo
+else
+    echo "Building docker image from local repo assuming the script was run from dataflo/python..."
+fi
+
+GIT_SHA=$(git rev-parse $GIT_BRANCH)
+# Login to ECR.
+aws ecr get-login-password --region $ECR_REGION|docker login --username AWS --password-stdin $ECR_HOST
+docker build -t "$ECR_REPO_FQN:$GIT_SHA"  -t "$ECR_REPO_FQN:$PLATFORM-latest" -f "$DOCKER_FILE_PATH" .
+
+if [ ! $NO_PUSH ] && [ ! $LOCAL ]; then
+    docker push -a "$ECR_REPO_FQN"
+else
+    echo "Not pushing the docker image to ECR because either --no-push or --local was passed as a flag."
+fi
+
+# Clean up old (now untagged) images.
+docker system prune --force
---
+++ docker/scripts/refresh_git_config.py
@@ -0,0 +1,60 @@
+import logging
+import os
+import re
+import subprocess
+import sys
+
+import requests
+
+"""
+This script runs as a cron job in the IDE docker container, so make sure we don't add any dependencies
+other than core python
+"""
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def update_git_config(access_token):
+    logging.info("Getting the current url for the repo in current directory...")
+    output = subprocess.run(
+        ["git", "remote", "get-url", "origin"], capture_output=True, text=True
+    )
+    if output.returncode == 0:
+        url = output.stdout.strip()
+    else:
+        logging.error("Failed to update the access_token.")
+    logging.info(f"The URL for the repo is {url}")
+    new_url = re.sub(
+        'x-access-token:[a-zA-z0-9]*@', f'x-access-token:{access_token}@', url
+    )
+    logging.info(f"Replacing the URL with {new_url}.")
+    output = subprocess.run(
+        ["git", "remote", "set-url", "origin", new_url], capture_output=True, text=True
+    )
+    if output.returncode == 0:
+        return True
+    logging.error("Failed to update the access_token.")
+    return False
+
+
+def main():
+    headers = {
+        'Authorization': 'Token ' + os.environ['INTERNAL_SERVER_API_TOKEN'],
+        'X-Auth-User-Id': os.environ['DFCTL_USER_ID'],
+    }
+    server_url = os.environ['DFCTL_API_ENDPOINT'] + "/github_app_access_token"
+    logging.info(
+        f"Requesting token from {server_url} for user {os.environ['DFCTL_USER_ID']}"
+    )
+    res = requests.get(server_url, headers=headers)
+    if res.status_code != 200:
+        logging.error(
+            f"Failed to fetch a new access token from {os.environ['DFCTL_API_ENDPOINT']}"
+        )
+        res.raise_for_status()
+    output = res.json()
+    update_git_config(output['access_token'])
+
+
+if __name__ == '__main__':
+    main()
