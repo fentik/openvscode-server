7fbfef8f8e20fc0e2d084bdd8125421422dcf5e4
Sergei Tsarev <sergei@fentik.com>
2022-06-04 08:59:57 -0700

    add fentik vscode extension

--- docker/scripts/ide_server_init.sh
+++ docker/scripts/ide_server_init.sh
@@ -93,6 +93,7 @@ cat <<EOF > $VSCODE_WORKSPACE/.vscode/settings.json
 EOF
 
 cd /opt/vscode-reh-web
+./bin/openvscode-server --install-extension /opt/openvscode-server/binary-dependencies/vscode-fentik-0.0.1.vsix
 ./bin/openvscode-server --install-extension /opt/openvscode-server/binary-dependencies/sqltools-0.24.0.vsix
 ./bin/openvscode-server --install-extension /opt/openvscode-server/binary-dependencies/sqltools-driver-fentik-0.1.0.vsix
 ./bin/openvscode-server --port 3001 --host 0.0.0.0 --without-connection-token --default-folder $VSCODE_WORKSPACE --default-workspace $VSCODE_WORKSPACE
